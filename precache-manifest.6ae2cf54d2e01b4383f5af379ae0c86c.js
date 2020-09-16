bce333162e70b7ba7bb8f15098da649d552b94d0
Gwen <gwenaelle.bourhis3@gmail.com>
2020-09-16 10:35:27 +0200

    Updates

---
+++ precache-manifest.6ae2cf54d2e01b4383f5af379ae0c86c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "081c0d0512d592f8ba8899b56017b5d6",
+    "url": "/Jeu-du-pendu-React/index.html"
+  },
+  {
+    "revision": "d065891c5e3e11616d2e",
+    "url": "/Jeu-du-pendu-React/static/css/main.d2dd4d25.chunk.css"
+  },
+  {
+    "revision": "8cb629964d02d507b59d",
+    "url": "/Jeu-du-pendu-React/static/js/2.92f707c9.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Jeu-du-pendu-React/static/js/2.92f707c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d065891c5e3e11616d2e",
+    "url": "/Jeu-du-pendu-React/static/js/main.b3830717.chunk.js"
+  },
+  {
+    "revision": "2668a6cee7ae3b650a8c",
+    "url": "/Jeu-du-pendu-React/static/js/runtime-main.b501fa57.js"
+  }
+]);
